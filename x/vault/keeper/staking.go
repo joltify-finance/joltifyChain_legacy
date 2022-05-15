60fd8ff4207abc168280394a49c7f258fa47841b
Ford Betelgeuse <ford.betelgeusedent@gmail.com>
2022-05-15 23:32:39 +1000

    remote useless staking code

--- x/vault/keeper/staking.go
+++ x/vault/keeper/staking.go
@@ -2,14 +2,15 @@ package keeper
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
+	"time"
+
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	abci "github.com/tendermint/tendermint/abci/types"
-	"sort"
-	"strconv"
-	"time"
 
 	vaulttypes "gitlab.com/joltify/joltifychain/x/vault/types"
 )
@@ -108,125 +109,6 @@ func (k Keeper) getEligibleValidators(ctx sdk.Context) ([]vaulttypes.ValidatorPo
 	return candidates, nil
 }
 
-//
-//func (k Keeper) ApplyAndReturnValidatorSetUpdates(ctx sdk.Context) (updates []abci.ValidatorUpdate, err error) {
-//	maxValidators := k.vaultStaking.GetParams(ctx).MaxValidators
-//	_ = maxValidators
-//	totalPower := sdk.ZeroInt()
-//	amtFromBondedToNotBonded, amtFromNotBondedToBonded := sdk.ZeroInt(), sdk.ZeroInt()
-//
-//	// Retrieve the last validator set.
-//	// The persistent set is updated later in this function.
-//	// (see LastValidatorPowerKey).
-//	last, err := k.getLastValidatorByAddr(ctx)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	// Iterate over validators, and get the power of the nodes.
-//	allValidators, err := k.getEligibleValidators(ctx)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	for _, validator := range allValidators {
-//		// everything that is iterated in this loop is becoming or already a
-//		// part of the bonded validator set
-//
-//		if validator.Jailed {
-//			panic("should never retrieve a jailed validator from the power store")
-//		}
-//
-//		// if we get to a zero-power validator (which we don't bond),
-//		// there are no more possible bonded validators
-//		if validator.PotentialConsensusPower(sdk.DefaultPowerReduction) == 0 {
-//			break
-//		}
-//
-//		// apply the appropriate state change if necessary
-//		switch {
-//		case validator.IsUnbonded():
-//			validator, err = k.unbondedToBonded(ctx, validator)
-//			if err != nil {
-//				return
-//			}
-//			amtFromNotBondedToBonded = amtFromNotBondedToBonded.Add(validator.GetTokens())
-//		case validator.IsUnbonding():
-//			validator, err = k.unbondingToBonded(ctx, validator)
-//			if err != nil {
-//				return
-//			}
-//			amtFromNotBondedToBonded = amtFromNotBondedToBonded.Add(validator.GetTokens())
-//		case validator.IsBonded():
-//			// no state change
-//		default:
-//			panic("unexpected validator status")
-//		}
-//
-//		// fetch the old power bytes
-//		valAddr, err := sdk.ValAddressFromBech32(validator.OperatorAddress)
-//		if err != nil {
-//			panic("the validator Operator address should never fail")
-//		}
-//
-//		oldPowerBytes, found := last[validator.OperatorAddress]
-//		newPower := validator.ConsensusPower(sdk.DefaultPowerReduction)
-//		newPowerBytes := k.cdc.MustMarshal(&gogotypes.Int64Value{Value: newPower})
-//
-//		// update the validator set if power has changed
-//		if !found || !bytes.Equal(oldPowerBytes, newPowerBytes) {
-//			updates = append(updates, validator.ABCIValidatorUpdate(sdk.DefaultPowerReduction))
-//			k.vaultStaking.SetLastValidatorPower(ctx, valAddr, newPower)
-//		}
-//
-//		delete(last, validator.OperatorAddress)
-//
-//		totalPower = totalPower.Add(sdk.NewInt(newPower))
-//	}
-//
-//	noLongerBonded, err := sortNoLongerBonded(last)
-//	if err != nil {
-//		return nil, err
-//	}
-//	for _, valAddrBytes := range noLongerBonded {
-//		validator := k.mustGetValidator(ctx, sdk.ValAddress(valAddrBytes))
-//		consAddr, errCons := validator.GetConsAddr()
-//		if errCons != nil {
-//			panic("should not fail to get the cons address")
-//		}
-//		delete(k.validatorStandbyPowerInfo, consAddr.String())
-//		validator, err = k.bondedToUnbonding(ctx, validator)
-//		if err != nil {
-//			return
-//		}
-//		amtFromBondedToNotBonded = amtFromBondedToNotBonded.Add(validator.GetTokens())
-//		k.vaultStaking.DeleteLastValidatorPower(ctx, validator.GetOperator())
-//		updates = append(updates, validator.ABCIValidatorUpdateZero())
-//	}
-//
-//	// Update the pools based on the recent updates in the validator set:
-//	// - The tokens from the non-bonded candidates that enter the new validator set need to be transferred
-//	// to the Bonded pool.
-//	// - The tokens from the bonded validators that are being kicked out from the validator set
-//	// need to be transferred to the NotBonded pool.
-//	switch {
-//	// Compare and subtract the respective amounts to only perform one transfer.
-//	// This is done in order to avoid doing multiple updates inside each iterator/loop.
-//	case amtFromNotBondedToBonded.GT(amtFromBondedToNotBonded):
-//		k.notBondedTokensToBonded(ctx, amtFromNotBondedToBonded.Sub(amtFromBondedToNotBonded))
-//	case amtFromNotBondedToBonded.LT(amtFromBondedToNotBonded):
-//		k.bondedTokensToNotBonded(ctx, amtFromBondedToNotBonded.Sub(amtFromNotBondedToBonded))
-//	default: // equal amounts of tokens; no update required
-//	}
-//
-//	// set total power on lookup index if there are any updates
-//	if len(updates) > 0 {
-//		k.vaultStaking.SetLastTotalPower(ctx, totalPower)
-//	}
-//
-//	return updates, err
-//}
-
 func (k Keeper) updateValidators(ctx sdk.Context) error {
 	vs, err := k.getEligibleValidators(ctx)
 	if err != nil {
@@ -255,88 +137,6 @@ func (k Keeper) updateValidators(ctx sdk.Context) error {
 	return nil
 }
 
-//func (k Keeper) BlockValidatorUpdates(ctx sdk.Context) []abci.ValidatorUpdate {
-//	validatorUpdates, err := k.ApplyAndReturnValidatorSetUpdates(ctx)
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	// unbond all mature validators from the unbonding queue
-//	k.vaultStaking.UnbondAllMatureValidators(ctx)
-//
-//	// Remove all mature unbonding delegations from the ubd queue.
-//	matureUnbonds := k.vaultStaking.DequeueAllMatureUBDQueue(ctx, ctx.BlockHeader().Time)
-//	for _, dvPair := range matureUnbonds {
-//		addr, err := sdk.ValAddressFromBech32(dvPair.ValidatorAddress)
-//		if err != nil {
-//			panic(err)
-//		}
-//		delegatorAddress, err := sdk.AccAddressFromBech32(dvPair.DelegatorAddress)
-//		if err != nil {
-//			panic(err)
-//		}
-//		balances, err := k.vaultStaking.CompleteUnbonding(ctx, delegatorAddress, addr)
-//		if err != nil {
-//			continue
-//		}
-//
-//		newEvent := sdk.NewEvent(
-//			stakingtypes.EventTypeCompleteUnbonding,
-//			sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
-//			sdk.NewAttribute(types.AttributeKeyValidator, dvPair.ValidatorAddress),
-//			sdk.NewAttribute(stakingtypes.AttributeKeyDelegator, dvPair.DelegatorAddress),
-//		)
-//
-//		typedEvent, err := sdk.ParseTypedEvent(abci.Event(newEvent))
-//		if err != nil {
-//			k.Logger(ctx).Error("fail to parse the event")
-//		}
-//		ctx.EventManager().EmitTypedEvent(typedEvent)
-//	}
-//
-//	// Remove all mature redelegations from the red queue.
-//	matureRedelegations := k.vaultStaking.DequeueAllMatureRedelegationQueue(ctx, ctx.BlockHeader().Time)
-//	for _, dvvTriplet := range matureRedelegations {
-//		valSrcAddr, err := sdk.ValAddressFromBech32(dvvTriplet.ValidatorSrcAddress)
-//		if err != nil {
-//			panic(err)
-//		}
-//		valDstAddr, err := sdk.ValAddressFromBech32(dvvTriplet.ValidatorDstAddress)
-//		if err != nil {
-//			panic(err)
-//		}
-//		delegatorAddress, err := sdk.AccAddressFromBech32(dvvTriplet.DelegatorAddress)
-//		if err != nil {
-//			panic(err)
-//		}
-//		balances, err := k.vaultStaking.CompleteRedelegation(
-//			ctx,
-//			delegatorAddress,
-//			valSrcAddr,
-//			valDstAddr,
-//		)
-//		if err != nil {
-//			continue
-//		}
-//
-//		newEvent := sdk.NewEvent(
-//			stakingtypes.EventTypeCompleteRedelegation,
-//			sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
-//			sdk.NewAttribute(stakingtypes.AttributeKeyDelegator, dvvTriplet.DelegatorAddress),
-//			sdk.NewAttribute(stakingtypes.AttributeKeySrcValidator, dvvTriplet.ValidatorSrcAddress),
-//			sdk.NewAttribute(stakingtypes.AttributeKeyDstValidator, dvvTriplet.ValidatorDstAddress),
-//		)
-//
-//		typedEvent, err := sdk.ParseTypedEvent(abci.Event(newEvent))
-//		if err != nil {
-//			k.Logger(ctx).Error("fail to parse the event")
-//		}
-//		ctx.EventManager().EmitTypedEvent(typedEvent)
-//	}
-//
-//	return validatorUpdates
-//}
-//
 func (k Keeper) NewUpdate(ctx sdk.Context) []abci.ValidatorUpdate {
 	defer telemetry.ModuleMeasureSince(vaulttypes.ModuleName, time.Now(), telemetry.MetricKeyEndBlocker)
 
--- x/vault/types/expected_keepers.go
+++ x/vault/types/expected_keepers.go
@@ -1,77 +1,18 @@
 package types
 
 import (
-	"time"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 type VaultStaking interface {
-	IterateBondedValidatorsByPower(sdk.Context, func(index int64, validator stakingtypes.ValidatorI) (stop bool))
-
-	// IterateLastValidators iterate through the consensus validator set of the last block by operator address, execute func for each validator
 	IterateLastValidators(sdk.Context, func(index int64, validator stakingtypes.ValidatorI) (stop bool))
-
-	// IterateValidators iterate through validators by operator address, execute func for each validator
-	IterateValidators(sdk.Context, func(index int64, validator stakingtypes.ValidatorI) (stop bool))
 
 	ValidatorsPowerStoreIterator(ctx sdk.Context) sdk.Iterator
 
-	SetLastValidatorPower(ctx sdk.Context, operator sdk.ValAddress, power int64)
-
-	// update the block updates
-	BlockValidatorUpdates(ctx sdk.Context) []abci.ValidatorUpdate
-
-	// UnbondAllMatureValidators unbonds all the mature unbonding validators that
-	// have finished their unbonding period.
-	UnbondAllMatureValidators(ctx sdk.Context)
-
-	DequeueAllMatureUBDQueue(ctx sdk.Context, currTime time.Time) (matureUnbonds []stakingtypes.DVPair)
-
-	CompleteUnbonding(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.ValAddress) (sdk.Coins, error)
-
-	DequeueAllMatureRedelegationQueue(ctx sdk.Context, currTime time.Time) (matureRedelegations []stakingtypes.DVVTriplet)
-
-	CompleteRedelegation(ctx sdk.Context, delAddr sdk.AccAddress, valSrcAddr, valDstAddr sdk.ValAddress) (sdk.Coins, error)
-
 	GetParams(ctx sdk.Context) stakingtypes.Params
+
 	LastValidatorsIterator(ctx sdk.Context) (iterator sdk.Iterator)
 
-	DeleteValidatorByPowerIndex(ctx sdk.Context, validator stakingtypes.Validator)
-
-	SetValidator(ctx sdk.Context, validator stakingtypes.Validator)
-	SetValidatorByPowerIndex(ctx sdk.Context, validator stakingtypes.Validator)
-	DeleteValidatorQueue(ctx sdk.Context, val stakingtypes.Validator)
-	AfterValidatorBonded(ctx sdk.Context, consAddr sdk.ConsAddress, valAddr sdk.ValAddress)
 	GetValidator(ctx sdk.Context, addr sdk.ValAddress) (validator stakingtypes.Validator, found bool)
-	InsertUnbondingValidatorQueue(ctx sdk.Context, val stakingtypes.Validator)
-	DeleteLastValidatorPower(ctx sdk.Context, operator sdk.ValAddress)
-
-	SetLastTotalPower(ctx sdk.Context, power sdk.Int)
-
-	BondDenom(ctx sdk.Context) (res string)
-
-	GetHistoricalInfo(ctx sdk.Context, height int64) (stakingtypes.HistoricalInfo, bool)
-}
-
-// BankKeeper Methods imported from bank should be defined here
-type BankKeeper interface {
-	SendKeeper
-	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
-	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
-	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
-	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
-
-	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
-}
-
-type SendKeeper interface {
-	ViewKeeper
-}
-
-type ViewKeeper interface {
-	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 }
