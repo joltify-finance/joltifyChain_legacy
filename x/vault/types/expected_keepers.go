8f5d48f54377ad767b1ddc658d10d0104479c44d
George Miller <george.miller0042@gmail.com>
2022-05-16 15:25:18 +1000

    add the missing bank and staking functions

--- x/vault/types/expected_keepers.go
+++ x/vault/types/expected_keepers.go
@@ -15,4 +15,25 @@ type VaultStaking interface {
 	LastValidatorsIterator(ctx sdk.Context) (iterator sdk.Iterator)
 
 	GetValidator(ctx sdk.Context, addr sdk.ValAddress) (validator stakingtypes.Validator, found bool)
+
+	GetHistoricalInfo(ctx sdk.Context, height int64) (stakingtypes.HistoricalInfo, bool)
+}
+
+// BankKeeper Methods imported from bank should be defined here
+type BankKeeper interface {
+	SendKeeper
+	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+}
+
+type SendKeeper interface {
+	ViewKeeper
+}
+type ViewKeeper interface {
+	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 }
